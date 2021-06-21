71acaeceef3cd2c7cc75d305003d38838439d038
Jade Carvalho <62659009+Jade-ux@users.noreply.github.com>
2021-06-21 10:50:30 +0100

    Update registration.js

--- static/js/registration.js
+++ static/js/registration.js
@@ -1,25 +1,13 @@
 "use strict";
 
-const countryRestrict = {
-  country: "uk",
-};
+document.getElementById("autocomplete").addEventListener("click", initialize)
 
-  uk: {
-    center: {
-      lat: 54.8,
-      lng: -4.6,
-    },
-    zoom: 5,
-  },
-
-let autocomplete;
-
-  //Create the autocomplete object and associate it with the 'cities' search box.
-  //Restrict the search to the country selected and place type to 'cities'.
-  autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById("autocomplete"),
-    {
-      types: ["(cities)"],
-      componentRestrictions: countryRestrict,
-    }
-  );
+function initialize() {
+    var input = document.getElementById('autocomplete');
+    var options = {
+    componentRestrictions: { country: ["gb", "ie"] },
+    types: ['geocode'] //this should work !
+    };
+    var autocomplete = new google.maps.places.Autocomplete(input, options);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
